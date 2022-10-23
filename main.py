c3b29e52c4e5cce19202b70c2739571ecf3053f7
the Milky Way <49054536+QInzhengk@users.noreply.github.com>
2022-10-23 15:17:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,72 @@
-from datetime import date, datetime
+import os
 import math
+import random
+import requests
+
+from datetime import date, datetime
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
-import requests
-import os
-import random
 
 today = datetime.now()
-start_date = os.environ['START_DATE']
 city = os.environ['CITY']
+#
+start_date = os.environ['START_DATE']
 birthday = os.environ['BIRTHDAY']
-
+# 微信公众测试号ID和SECRET
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
-
+# 用户ID和模板ID
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
+# 获取天气和温度
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+    res = requests.get(url).json()
+    weather = res['data']['list'][0]
+    return weather['weather'], math.floor(weather['temp'])
 
+
+# 距离设置的日期过了多少天
 def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+    return delta.days
 
+
+# 距离过生日还有多少天
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+    next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+    if next < datetime.now():
+        next = next.replace(year=next.year + 1)
+    return (next - today).days
 
+
+# 每日一句
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+    words = requests.get("https://api.shadiao.pro/chp")
+    if words.status_code != 200:
+        return get_words()
+    return words.json()['data']['text']
 
+
+# 字体随机颜色
 def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
+    return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
 client = WeChatClient(app_id, app_secret)
-
 wm = WeChatMessage(client)
+
 wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+
+data = {
+    "weather": {"value": wea, "color": get_random_color()},
+    "temperature": {"value": temperature, "color": get_random_color()},
+    "love_days": {"value": get_count(), "color": get_random_color()},
+    "birthday_left": {"value": get_birthday(), "color": get_random_color()},
+    "words": {"value": get_words(), "color": get_random_color()}
+        }
+
 res = wm.send_template(user_id, template_id, data)
 print(res)
