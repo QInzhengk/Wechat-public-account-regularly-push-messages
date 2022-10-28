88bc348775d2cbb6ff46f9166915bd95dce5d177
the Milky Way <49054536+QInzhengk@users.noreply.github.com>
2022-10-28 22:21:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def get_words():
 def get_random_color():
     return "#%06x" % random.randint(0, 0xFFFFFF)
 
-print(len(os.environ["USER_ID0"].split(',')))
+print(os.environ["USER_ID0"][0:27])
 exit()
 client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
